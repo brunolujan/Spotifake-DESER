61afa49e7238ac4c585e8324f899cebae7013f2a
Bruno Luján <bruno1529@live.com.mx>
2020-07-02 20:19:28 -0500

    Conexión a la base de datos

--- Server/SpotifakeServerHandler.py
+++ Server/SpotifakeServerHandler.py
@@ -3,7 +3,20 @@ import SpotifakeServer
 sys.path.append("gen-py")
 from SpotifakeService import SpotifakeService
 from SpotifakeService .ttypes import *
+from SQLConenection.connection import SQLConnection
 
 class SpotifakeServerHandler(SpotifakeService.Iface):
-    def GetConsumerById():
+
+    def __init__(self):
+        
+        self.connection = ConnectionSQL()
+
+    def GetConsumerById(idConsumer):
+
+        self.connection.open()
+
+        self.save()
+
+        self.close()
+
         return 1
