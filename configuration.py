69441a2e1a904fd8b89cbfd0b28045a639e20fc0
Bruno Luján <bruno1529@live.com.mx>
2020-07-04 13:32:02 -0500

    Actualización

--- configuration.py
+++ configuration.py
@@ -1,10 +1,6 @@
 import os
 
-<<<<<<< HEAD
 DATABASE_SERVER_IP = '192.168.0.3' 
-=======
-DATABASE_SERVER_IP = '172.22.202.89' 
->>>>>>> 2513789b5d23fc22248d3780ae13131abcac34c4
 DATABASE_NAME = 'Spotifake'
 DATABASE_USERNAME = 'userSpotifake'
 DATABASE_PASSWORD = 'Spotifake2020'
