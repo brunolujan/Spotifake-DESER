917184685c0e4d366665300ec147d47f43942ad6
BRUNO ANTONIO LOPEZ LUJAN <zS17021850@estudiantes.uv.mx>
2020-07-17 02:24:00 -0500

    Avance cliente

--- Client/Client/Client/AddAlbum.xaml.cs
+++ Client/Client/Client/AddAlbum.xaml.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.Win32;
+﻿using MahApps.Metro.Controls;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.UI.WebControls;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
@@ -13,6 +15,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static System.Windows.Forms.ComboBox;
+using Image = System.Windows.Controls.Image;
+using ListItem = System.Web.UI.WebControls.ListItem;
 
 namespace Client {
 
@@ -91,6 +96,16 @@ namespace Client {
             button_AddAlbum.Content = "ADD SINGLE";
             label_Title.Text = "NEW SINGLE";
             label_TrackMessage.Text = "TRACK";
+            label_SingleGender.Visibility = Visibility.Visible;
+            textBox_SingleTrackTitle.Visibility = Visibility.Visible;
+            label_SingleTitle.Visibility = Visibility.Visible;
+            comboBox_SingleTrackGender.Visibility = Visibility.Visible;
+            button_SingleSelectTrackFile.Visibility = Visibility.Visible;
+            foreach (int r in Enum.GetValues(typeof(MusicGender))) {
+                var items = new ListItem(Enum.GetName(typeof(MusicGender), r), r.ToString());
+                comboBox_Gender.Items.Add(items);
+                comboBox_SingleTrackGender.Items.Add(items);
+            }
         }
 
         private void LoadNewAlbum() {
@@ -111,11 +126,44 @@ namespace Client {
         }
 
         private void button_AddAlbum_Click(object sender, RoutedEventArgs e) {
+            if (thisVar == 0) {
 
+            } else {
+                if (ValidateBlankSpacesSingle() == true) {
+                    label_Message.Text = "*Complete all fields";
+                } else {
+
+                }
+            }
         }
 
         private void button_SelectTrackFile_Click(object sender, RoutedEventArgs e) {
 
+        }
+
+        private void button_SingleSelectTrackFile_Click(object sender, RoutedEventArgs e) {
+            try {
+                OpenFileDialog ofd = new OpenFileDialog();
+                var resultado = ofd.ShowDialog();
+                string path = "";
+                if (resultado == true) {
+                    path = ofd.FileName;
+                    var tfile = TagLib.File.Create(path);
+                    textBox_SingleTrackTitle.Text = tfile.Tag.Title;
+                    int duration = Convert.ToInt32(tfile.Properties.Duration.TotalSeconds);
+                }
+            } catch (Exception ex) {
+                Console.WriteLine(ex + "in AddAlbum bSingleSelectTrackFile");
+            }
+        }
+
+        private bool ValidateBlankSpacesSingle() {
+            if (textBox_AlbumTitle.Text == "" || comboBox_Featuring.SelectedIndex == -1 || comboBox_Gender.SelectedIndex == -1 || 
+                textBox_SingleTrackTitle.Text == "" || comboBox_SingleFeaturing.SelectedIndex == -1 || comboBox_SingleTrackGender.SelectedIndex == -1) {
+                return true;
+            } else {
+                return false;
+            }
         }
     }
 }
