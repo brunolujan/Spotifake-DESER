6ed01e41d13592124bf04b69f70c3696bfb25e79
MariaJoseHM99 <35043399+MariaJoseHM99@users.noreply.github.com>
2020-07-31 17:15:39 -0500

    Avance Implementacion Pages: Track, Album, Playlist, Track

--- Client/Client/Client/Main.xaml.cs
+++ Client/Client/Client/Main.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Client {
@@ -20,12 +21,24 @@ namespace Client {
     public partial class Main {
         public Main() {
             InitializeComponent();
+            centralFrame.NavigationUIVisibility = NavigationUIVisibility.Hidden;
         }
 
         private void button_Tracks_MouseDoubleClick(object sender, MouseButtonEventArgs e) {
             centralFrame.Navigate(new TracksPages());
         }
 
-  
+        private void button_ContentCreators_MouseDoubleClick(object sender, MouseButtonEventArgs e) {
+            centralFrame.Navigate(new ContentCreatorsPages());
+        }
+
+        private void button_Albums_MouseDoubleClick(object sender, MouseButtonEventArgs e) {
+            centralFrame.Navigate(new AlbumsPages());
+
+        }
+
+        private void button_Playlists_MouseDoubleClick(object sender, MouseButtonEventArgs e) {
+            centralFrame.Navigate(new PlaylistsPages());
+        }
     }
 }
