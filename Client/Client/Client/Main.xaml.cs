ac9b878a74d5b4ab6f57966637494708510551b7
MariaJoseHM99 <35043399+MariaJoseHM99@users.noreply.github.com>
2020-07-31 16:32:27 -0500

    Implementación TracksPage

---
+++ Client/Client/Client/Main.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Client.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Client {
+    /// <summary>
+    /// Lógica de interacción para Main.xaml
+    /// </summary>
+    public partial class Main {
+        public Main() {
+            InitializeComponent();
+        }
+
+        private void button_Tracks_MouseDoubleClick(object sender, MouseButtonEventArgs e) {
+            centralFrame.Navigate(new TracksPages());
+        }
+
+  
+    }
+}
