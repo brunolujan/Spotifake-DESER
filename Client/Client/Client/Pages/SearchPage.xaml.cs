59bcecf2a2e1f792f199805ec7b10e5245353ef9
MariaJoseHM99 <35043399+MariaJoseHM99@users.noreply.github.com>
2020-08-02 15:25:00 -0500

    Implementación Search

--- Client/Client/Client/Pages/SearchPage.xaml.cs
+++ Client/Client/Client/Pages/SearchPage.xaml.cs
@@ -24,6 +24,9 @@ namespace Client.Pages {
 
         private void HiddenLists() {
             datagrid_SearchTracks.Visibility = Visibility.Hidden;
+            datagrid_SearchAlbums.Visibility = Visibility.Hidden;
+            datagrid_SearchContentCreators.Visibility = Visibility.Hidden;
+            datagrid_SearchPlaylists.Visibility = Visibility.Hidden;
         }
 
         public async void GetTrackByQuery() {
@@ -31,7 +34,46 @@ namespace Client.Pages {
             {
                 datagrid_SearchTracks.Visibility = Visibility.Visible;
                 List<Track> tracks = await Session.serverConnection.trackService.GetTrackByQueryAsync(TextBox_search.Text);
-                datagrid_SearchTracks.ItemsSource = tracks.Select(x => new { NUM = x.TrackNumber, TITLE = x.Title, SECONDS = x.DurationSeconds}); 
+                datagrid_SearchTracks.ItemsSource = tracks.Select(x => new { NUM = x.TrackNumber, TITLE = x.Title, SECONDS = x.DurationSeconds });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Please try again");
+            }
+        }
+
+        public async void GetAlbumByQuery() {
+            try
+            {
+                datagrid_SearchAlbums.Visibility = Visibility.Visible;
+                List<Album> albums = await Session.serverConnection.albumService.GetAlbumByQueryAsync(TextBox_search.Text);
+                datagrid_SearchAlbums.ItemsSource = albums;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Please try again");
+            }
+        }
+
+        public async void GetContentCreatorByQuery() {
+            try
+            {
+                datagrid_SearchContentCreators.Visibility = Visibility.Visible;
+                List<ContentCreator> contentCreators = await Session.serverConnection.contentCreatorService.GetContentCreatorByQueryAsync(TextBox_search.Text);
+                datagrid_SearchContentCreators.ItemsSource = contentCreators.Select(x => new { NAME = x.StageName, DESCRIPTION = x.Description });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Please try again");
+            }
+        }
+
+        public async void GetPlaylistByQuery() {
+            try
+            {
+                datagrid_SearchPlaylists.Visibility = Visibility.Visible;
+                List<Playlist> playlists = await Session.serverConnection.playlistService.GetPlaylistByQueryAsync(TextBox_search.Text);
+                datagrid_SearchPlaylists.ItemsSource = playlists.Select(x => new { NAME = x.Name, DESCRIPTION = x.Description });
             }
             catch (Exception ex)
             {
@@ -61,6 +103,18 @@ namespace Client.Pages {
                     case "Tracks":
                         HiddenLists();
                         GetTrackByQuery();
+                        break;
+                    case "Albums":
+                        HiddenLists();
+                        GetAlbumByQuery();
+                        break;
+                    case "Artists":
+                        HiddenLists();
+                        GetContentCreatorByQuery();
+                        break;
+                    case "Playlists":
+                        HiddenLists();
+                        GetPlaylistByQuery();
                         break;
                 }
             }
